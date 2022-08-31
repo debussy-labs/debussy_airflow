cd150b68d3ede0ee99da8a40948b2ae4c912ddc8
Lawrence Fernandes <lawrence.fernandes@dotz.com>
2022-08-31 20:08:46 -0300

    Update setup.cfg

--- setup.py
+++ setup.py
@@ -2,12 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name="debussy_airflow",
-    version="0.1.0",
+    version="0.0.1",
+    author="Lawrence Fernandes",
+    author_email="lawrence.stfs@gmail.com",
+    description="Debussy Concert provider for Airflow",
+    license="Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)",
+    url='https://github.com/DotzInc/debussy_airflow',
     packages=find_packages(),
     install_requires=[
         "apache-airflow-providers-google==8.0.0",
         "facebook-business==13.0.0",
         "mysql-connector-python==8.0.24",
         "paramiko==2.8.1"
-    ]
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7'
 )
