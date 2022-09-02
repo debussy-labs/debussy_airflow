bfd236a01d36efa0a90e38885b9204a105ef0ca4
Lawrence Fernandes <lawrence.fernandes@dotz.com>
2022-09-02 17:35:26 -0300

    Moved config to setup.cfg, added requirements.txt and CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+0.0.1 - First release
+0.0.2 - Fix naming conventions for hooks, operators and sensors, and updated example_dags
+0.0.3 - Improved README description, and setup.py
+0.0.4 - Moved config to setup.cfg, added requirements.txt and CHANGELOG
--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name="debussy_airflow",
-    version="0.0.3",
-    author="Lawrence Fernandes",
-    author_email="lawrence.stfs@gmail.com",
-    description="Debussy Concert provider for Airflow",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    license="Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)",
-    url='https://github.com/DotzInc/debussy_airflow',
-    packages=find_packages('debussy_airflow'),
-    package_dir={'': 'debussy_airflow'},
-    install_requires=[
-        "apache-airflow-providers-google==8.0.0",
-        "facebook-business==13.0.0",
-        "mysql-connector-python==8.0.24",
-        "paramiko==2.8.1"
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7'
-)
+setup()
