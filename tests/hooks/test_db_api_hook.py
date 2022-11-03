e4335ee51d32106c934e6841ed07fea62c40f339
maycon.silva <maycon.silva@dotzmkt.com.br>
2022-11-03 12:52:33 -0300

    set import test of existent folder

--- tests/hooks/test_db_api_hook.py
+++ tests/hooks/test_db_api_hook.py
@@ -1,3 +1,4 @@
+import logging
 from uuid import uuid4
 from airflow import DAG
 
@@ -9,21 +10,48 @@ from tests.test_tools import TestHookOperator, test_dag
 
 pgsql_hook = PostgreSQLConnectorHook(rdbms_conn_id="connection_pgsql_dev")
 
-def test_query_run_empty_list(context, pgsql_hook:DbApiHookInterface, dql_statement: str):
+def test_exec_query(context, pgsql_hook:DbApiHookInterface, dql_statement: str):
 
     assert (
         pgsql_hook.query_run(sql=dql_statement, autocommit=False) == None
         or len(pgsql_hook.query_run(sql=dql_statement, autocommit=False)) == 0
     )
-    
-with test_dag("test_debussy_pgsql_connector_dag") as dag:
-    test_http_run = TestHookOperator(
-        task_id="DQL_pgsql_test", execute_fn=test_query_run_empty_list
+        
+def test_get_first_record(context, pgsql_hook:DbApiHookInterface, dql_statement: str):
+    first_record = pgsql_hook.get_first(sql=dql_statement)
+    logging.info(f"valor: {first_record[0]}")
+    logging.info(f"tipo: {type(first_record[0])}")
+    assert (
+        first_record[0] != None
     )
-    query = ("SELECT COLUMN_NAME, IS_NULLABLE, DATA_TYPE FROM information_schema.COLUMNS"
-            f" WHERE TABLE_NAME = 'test_2' AND TABLE_SCHEMA = 'public'")
-    insert_query = f"INSERT INTO public.test_2(guid, pid, storecode, cpf_cnpj, valor, status, obs) VALUES('{uuid4()}', 0, '', '', 0, '', '');"
-    test_http_run.fn_kwargs = {
+
+with test_dag("test_debussy_pgsql_connector_dag") as dag:
+    test_insert_query_run = TestHookOperator(
+        task_id="test_insert_query_run", execute_fn=test_exec_query
+    )
+    pk = uuid4()
+    insert_query = f"INSERT INTO public.test_2(guid, pid, storecode, cpf_cnpj, valor, status, obs) VALUES('{pk}', 0, '', '', 0, '', '');"
+    test_insert_query_run.fn_kwargs = {
         "pgsql_hook": pgsql_hook,
-        "dql_statement": insert_query#"SELECT guid, pid, storecode, cpf_cnpj, valor, status, obs FROM public.fila_bonificacao;",
+        "dql_statement": insert_query
+    }
+
+    test_update_query_run = TestHookOperator(
+        task_id="test_update_query_run", execute_fn=test_exec_query
+    )
+    
+    update_query = f"UPDATE public.test_2 SET obs = 'updated field' WHERE guid='{pk}'"
+    test_update_query_run.fn_kwargs = {
+        "pgsql_hook": pgsql_hook,
+        "dql_statement": update_query
+    }
+
+    test_first_record = TestHookOperator(
+        task_id="test_first_record", execute_fn=test_get_first_record
+    )
+    
+    get_first_record_query = f"SELECT * FROM public.test_2"
+    test_first_record.fn_kwargs = {
+        "pgsql_hook": pgsql_hook,
+        "dql_statement": get_first_record_query
     }
